00f7ea101e374245fcd932819e5f38ab9cb1a196
Bruno Carvalho <34915606+Space-Cowb0y@users.noreply.github.com>
2024-01-09 18:06:22 -0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -2,30 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import urllib3
-
+j=[]
+resp=''
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 main_find = '<script defer="defer"'
 html = requests.get("https://conexo.ws",verify=False).content
 soup = BeautifulSoup(html, 'html.parser')
 html = soup.prettify()
-for i in html:
-    if i.__contains__(main_find):
-        print(i)
+for img in soup.findAll('script'):
+    src = img.get("src")
+print(src)
+
     
     
     
-html = requests.get("https://conexo.ws/static/js/main.b951ff3d.js",verify=False).content 
+html = requests.get("https://conexo.ws"+src,verify=False).content 
 
 soup = BeautifulSoup(html, 'html.parser')
 
 html = soup.prettify()
+
+
 
 index_S = html.find("ir=JSON.parse('") + len("ir=JSON.parse('")
 index_E = html.find("'),ur={")
 resposta = html[index_S:index_E]
 
 y = json.dumps(resposta, separators=(",", ":"))    
-with open("C:\\Users\\e5693423\\OneDrive - FIS\\Documents\\proj\\campeonato_conexo\\data.json", "w") as text_file:
+with open("C:\\Users\\Public\\proj\\campeonato_conexo\\data.json", "w") as text_file:
     a = json.dump(resposta, text_file, indent=1)
             
+
+
 

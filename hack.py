b0f9703ab52458989a87e0315ca945396721e7e9
Bruno Carvalho <34915606+Space-Cowb0y@users.noreply.github.com>
2024-01-09 21:04:42 -0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -11,27 +11,29 @@ soup = BeautifulSoup(html, 'html.parser')
 html = soup.prettify()
 for img in soup.findAll('script'):
     src = img.get("src")
-print(src)
-
-    
-    
-    
 html = requests.get("https://conexo.ws"+src,verify=False).content 
-
 soup = BeautifulSoup(html, 'html.parser')
-
 html = soup.prettify()
-
-
-
 index_S = html.find("ir=JSON.parse('") + len("ir=JSON.parse('")
 index_E = html.find("'),ur={")
 resposta = html[index_S:index_E]
 
+for i in resposta:
+    if ascii(i) == 92:
+        i.replace(i,'')
+    if i == '\\':
+        i.replace('\\','')
+    if i == '\\\\':
+        i.replace('\\\\','')
+    if i == '\\n':
+        i.replace('\\n','')
+    if i == '\\t':
+        i.replace('\\t','')
+
+
 y = json.dumps(resposta, separators=(",", ":"))    
-with open("data.json", "w") as text_file:
+with open("C:\\Users\\e5693423\\OneDrive - FIS\\Documents\\proj\\campeonato_conexo\\data.json", "w") as text_file:
     a = json.dump(resposta, text_file, indent=1)
+    
             
-
-
 
